16a8838c9791dee1a3d2c1e9a1e9296cda52f556
David Pollard <davidpollardcodingdojo@gmail.com>
2022-04-26 16:10:22 -0400

    commit for Likes exercise

---
+++ WebFund/d7/Likes/Likes.js
@@ -0,0 +1,10 @@
+
+function addlike(){
+    document.querySelector(".likes1").innerHTML++
+}
+function addlike2(){
+    document.querySelector(".likes2").innerHTML++
+}
+function addlike3(){
+    document.querySelector(".likes3").innerHTML++
+}
